151f72092b87a90ff96f6297a91e7ecc5023f2b4
DeadeyeDon <96637891+DeadeyeDon@users.noreply.github.com>
2021-12-27 20:01:37 -0600

    Update Order.cs

--- CSharp/Order.cs
+++ CSharp/Order.cs
@@ -1,9 +1,10 @@
 public class Order
 {
     public long ID { get; set; }
-    public Customer customer { get; set; }
-    public list<OrderDetail> orderDetails { get; set; }
-    public decimal totalAmount { get; set; }
+    public string customerName { get; set; }
+    public string itemSKU { get; set; }
+    public int itemQty { get; set; }
+    public decimal itemPrice { get; set; }
     public string currencyCode { get; set; }
     // order status (e.g. PENDING, ACTIVE, CANCELLED, COMPLETED)
     public string status { get; set; }
