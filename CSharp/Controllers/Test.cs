d29bd98d5991993e9090ba2b11cfb3a87d3175d3
DeadeyeDon <96637891+DeadeyeDon@users.noreply.github.com>
2021-12-27 20:14:14 -0600

    Update Test.cs

--- CSharp/Controllers/Test.cs
+++ CSharp/Controllers/Test.cs
@@ -10,12 +10,34 @@ namespace CSharp.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class Test : ControllerBase
+	public class OrderController : ControllerBase
 	{ 
+	        private static List<Order> orders = new List<Order> {
+            		new Order { ID = 1, customerName = "Dude",   },
+            		new Order { Id= 2, Name = "Batman"    },
+            		new Order { Id= 3, Name = "Spiderman" }
+        	};
+		
 		[HttpGet]
-		public string Get()
+		public ActionResult<List<Order>> Get()
 		{
 			return "Success!";
+		}
+		
+		[HttpPost]
+		public ActionResult Create(Order newOrder)
+		{
+		    var existingSuperheroItem = Superheroes.Find(x => x.Id == superheroItem.Id)
+		    if (existingSuperheroItem != null)
+		    {
+			return Conflict("Cannot create the Id because it already exists.");
+		    }
+		    else
+		    {
+			Superheroes.Add(superheroItem);
+			var resourceUrl = Request.Path.ToString() + '/' + superheroItem.Id;
+			return Created(resourceUrl, superheroItem);
+		    }
 		}
 	}
 }
